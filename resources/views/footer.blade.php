8b960ca2ef1b05c9647cc6664bba3fec2769e462
nafilah17 <rissanafilah@gmail.com>
2020-07-13 22:07:00 +0700

    new project include template blog admin

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" href="/assets/jquery/jquery.min.js"></script>
+  <script type="text/javascript" href="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
